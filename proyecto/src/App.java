39494b1b605078fb596637693389d2222da2e119
cristiancrakl <moralescadenac@gmail.com>
2024-10-18 13:20:40 -0500

    añdadi cosas

--- proyecto/src/App.java
+++ proyecto/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+    }
+
+    public static int intercambiar(int a,int b){
+        return a+b;
     }
 }
