f9b4bcdae9c3e27cd7f71bcc1cc7485d94c893ea
cristiancrakl <moralescadenac@gmail.com>
2024-10-18 13:25:53 -0500

    añdadi cosas

--- proyecto/src/App.java
+++ proyecto/src/App.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class App {
+
+    Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
+
         
+
     }
 
-    public static int intercambiar(int a,int b){
-        return a+b;
+    public static int intercambiar(int a, int b) {
+        return a + b;
+
     }
+
 }
