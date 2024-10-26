86c0ee731a732c03cabba8065781da8457bc68c6
cristiancrakl <moralescadenac@gmail.com>
2024-10-25 21:50:13 -0500

    hice lo de el numero mayor de 3 con metodos

--- Numero_mayor.java
+++ Numero_mayor.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 
 public class Numero_mayor {
-    
-    Scanner scanner=new Scanner(System.in);
 
-    
-    
-    public static int mayorNumero(int a,int b, int c){
-
-        return Math.max(a, Math.max(b, c));
-
-    }
-    
-    
-    public static void main(String[] args) {
-        int mayor= mayorNumero(1,2,3);
-        System.out.println("el numeor mayor es "+mayor);
-
+    public static void mayorNumero(){
+        Scanner scanner = new Scanner(System.in);
         
+        System.out.println("ingrese tres numeros para saber cual es el mayor");
+        double a =scanner.nextDouble();
+        double b =scanner.nextDouble();
+        double c  =scanner.nextDouble();
+        
+        double Numero_mayor=Math.max(a, Math.max(b, c));
+        
+
+         System.out.println("el numero mayor es: "+Numero_mayor);
+    }
+
+    public static void main(String[] args) {
+
+        mayorNumero();
+
     }
 
 }
